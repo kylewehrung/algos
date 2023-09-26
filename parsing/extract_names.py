15f917878c56fe51b763ca89e32c55a647eb1752
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-09-25 23:38:16 -0600

    working with spaCy to extract names

---
+++ parsing/extract_names.py
@@ -0,0 +1,30 @@
+import sys
+import spacy
+
+def extract_names(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    
+    first_name = ""
+    last_name = ""
+    
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            names = ent.text.split()
+            if len(names) >= 2:
+                first_name = names[0]
+                last_name = names[-1]
+                break
+
+    return first_name, last_name
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python extract_names.py 'Resume text here'")
+        sys.exit(1)
+
+    resume_text = sys.argv[1]
+    first_name, last_name = extract_names(resume_text)
+    print("First Name:", first_name)
+    print("Last Name:", last_name)
+
